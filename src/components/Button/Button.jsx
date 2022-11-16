0c470ec053345a526636726dadeeba0694d1a38e
Issei Mori <isseieikisouya@outlook.jp>
2022-11-16 17:17:03 +0900

    イベントの検知、argTypeによる

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,16 @@
 import './Button.css';
 import PropTypes from 'prop-types';
 
-function Button({ children, color = 'default', size = 'base',backgroundColor, }) {
-  return <button className={`${color} ${size}`} style={backgroundColor && { backgroundColor }}>{children}</button>;
+function Button({ children, color = 'default', size = 'base',backgroundColor, handleClick}) {
+  return <button className={`${color} ${size}`} style={backgroundColor && { backgroundColor }} onClick={handleClick}>{children}</button>;
 }
   
 export default Button;
+
+Button.propTypes = {
+  color: PropTypes.oneOf(['primary', 'default', 'danger']),
+  size: PropTypes.oneOf(['sm', 'base', 'lg']),
+  onClick: PropTypes.func.isRequired,
+};
 
 //参照https://reffect.co.jp/html/storybook#Args
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -16,6 +16,7 @@ export default {
     backgroundColor: {
       control: { type: 'color' },
     },
+    handleClick: { action: 'clicked' }
   },
 };
 
