3b90feb70baa52fa1a9367687b7681b21e3168f5
Issei Mori <isseieikisouya@outlook.jp>
2022-11-19 20:11:22 +0900

    おしまい！

--- src/components/Atoms/Balloon/index.stories.jsx
+++ src/components/Atoms/Balloon/index.stories.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Balloon from "./index.jsx";
 import "./index.jsx";
+import { withStyle } from "../../Utils/decorators.js";
+import{ withNotes }from '@storybook/addon-notes'
+import { addDecorator } from "@storybook/react";
 
 const Template = (args) => <Balloon {...args} />;
 
@@ -9,6 +12,8 @@ export default {
   title: 'Atoms/Balloon',
   component: Balloon,
 };
+
+const note = withNotes('読みやすさを考慮すると１０文字までが適当。１１文字以上を表示したい場合は別のデザインを考える');
 
 export const twoBalloon = () => <Balloon>次へ</Balloon>;
 export const fourBalloon = () => <Balloon>jjjj</Balloon>
